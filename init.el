09ac44efbe857816872f6dbb80c05fb10641b9b7
Lingchao Xin <douglarek@gmail.com>
2014-10-22 18:49:49 +0800

    Settings enhancement and cleanups

--- init.el
+++ init.el
@@ -44,11 +44,25 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
 (package-initialize)
 
+(unless package-archive-contents    ;; Refresh the packages descriptions
+  (package-refresh-contents))
+(setq package-load-list '(all))     ;; List of packages to load
+(unless (package-installed-p 'org)
+  (package-install 'org))
+(unless (package-installed-p 'jedi)
+  (package-install 'jedi))
+(unless (package-installed-p 'semx)
+  (package-install 'smex))
+
+;; Jedi -> Python auto-completion for Emacs.
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
