9cfef4f5e9d6b283fbc3715af086af08293ec5c2
Lingchao Xin <douglarek@gmail.com>
2014-10-24 23:58:40 +0800

    Refactored

---
+++ lisp/init-melpa.el
@@ -0,0 +1,62 @@
+;;;;;;;;;;;;;;;;;;;;;;;; Package Installed ;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+;; Refresh the packages descriptions
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; List of packages to load
+(setq package-load-list '(all))
+
+;; Install all needed
+(package-install 'jedi)
+(package-install 'ein)
+(package-install 'smex)
+(package-install 'whitespace-cleanup-mode)
+(package-install 'git-gutter)
+(package-install 'web-mode)
+
+;; jedi -> Python auto-completion for Emacs.
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+(setq jedi:complete-on-dot t)
+
+;; smex -> A smart M-x enhancement for Emacs.
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; whitespace-cleanup -> In Emacs, intelligently call whitespace-cleanup on save
+
+;; git-gutter -> Emacs port of GitGutter which is Sublime Text Plugin
+(global-git-gutter-mode t)
+
+;; web-mode -> web template editing mode for Emacs
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+)
+
+(provide 'init-melpa)
---
+++ lisp/init-self.el
@@ -0,0 +1,46 @@
+;;;;;;;;;;;;;;;;;;;;;;;; Self Settings ;;;;;;;;;;;;;;;;;;;;;;;;
+;; backup settings
+(defvar backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p backup-directory))
+        (make-directory backup-directory t))
+(setq backup-directory-alist `(("." . ,backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
+;; auto-save settings
+(defvar auto-save-directory (concat user-emacs-directory "auto-save"))
+(if (not (file-exists-p auto-save-directory))
+        (make-directory auto-save-directory t))
+
+(add-to-list 'auto-save-file-name-transforms
+             (list "\\(.+/\\)*\\(.*?\\)" (expand-file-name "\\2" auto-save-directory))
+             t)
+
+;; yes -> y, no -> n
+(defun my-yes-or-mumble-p (prompt)
+   "PROMPT user with a yes-or-no question, but only test for yes."
+   (if (string= "yes"
+                (downcase
+                 (read-from-minibuffer
+                  (concat prompt "(yes or no) "))))
+       t
+     nil))
+
+(defalias 'yes-or-no-p 'my-yes-or-mumble-p)
+
+;; disable startup message
+(setq inhibit-startup-message t)
+
+;; disable menu bar
+(menu-bar-mode -1)
+
+(provide 'init-self)
