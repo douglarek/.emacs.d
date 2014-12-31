447b3d9f46e7283d87b2e8fae2bdf3c758375ed8
Lingchao Xin <douglarek@gmail.com>
2014-12-31 23:19:31 +0800

    Cleanups

--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -10,12 +10,6 @@
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
 (package-initialize)
 
 ;; Refresh the packages descriptions
