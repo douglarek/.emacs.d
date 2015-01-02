98d33ed123792b74239b6632dff40b4913b447c6
Lingchao Xin <douglarek@gmail.com>
2015-01-02 15:21:42 +0800

    Revert "Confirm it when kill emacs"
    This reverts commit 1c257cc1ce2a76760596e01d248a216040551997.

--- lisp/init-self.el
+++ lisp/init-self.el
@@ -62,8 +62,5 @@
 ;; enable org indent on startup
 (setq org-startup-indented t)
 
-;; killing emacs is an import decision to me, so confirm it
-(setq confirm-kill-emacs 'yes-or-no-p)
-
 (provide 'init-self)
 ;;; init-self.el ends here
