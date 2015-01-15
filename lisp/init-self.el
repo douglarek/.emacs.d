6179d059e6da67452a30739a3727605b2e0dbd0e
Lingchao Xin <douglarek@gmail.com>
2015-01-15 14:22:49 +0800

    M-x run-python improved

--- lisp/init-self.el
+++ lisp/init-self.el
@@ -64,7 +64,18 @@
 
 ;; `run-python` use `ipython` instead `python` as much as possible
 (when (executable-find "ipython")
-  (setq python-shell-interpreter "ipython"))
+  (setq
+   python-shell-interpreter "ipython"
+   python-shell-interpreter-args ""
+   python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+   python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+   python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+   python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+   python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+)
 
 ;; when you visit a file, point goes to the last place where it was when you previously visited the same file
 (require 'saveplace)
