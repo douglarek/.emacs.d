5e46bd8bb81a1cd5d678feacdcd0c4e167c5c189
Lingchao Xin <douglarek@gmail.com>
2015-04-15 11:27:17 +0800

    An extension of Emacs that provides completion/syntax-check by using typescript-tools in typescrript-mode

--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -407,7 +407,22 @@
 ;; Erlang Development Tool Suite
 (add-hook 'after-init-hook 'my-after-init-hook)
 (defun my-after-init-hook ()
-    (require 'edts-start))
+  (require 'edts-start))
+
+
+;; Provide a interface for auto-complete.el/flymake.el on typescript-mode.
+(require 'typescript)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(require 'tss)
+
+;; Key binding
+(setq tss-popup-help-key "C-:")
+(setq tss-jump-to-definition-key "C->")
+(setq tss-implement-definition-key "C-c i")
+
+;; Do setting recommemded configuration
+(tss-config-default)
 
 
 (provide 'init-melpa)
