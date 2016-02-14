78cca6d34aaa78108e005e7cc08d7614364da8eb
Lingchao Xin <xinlingchao@meican.com>
2016-02-14 12:25:07 +0800

    Prompt before closing Emacs in a window system

--- lisp/init-self.el
+++ lisp/init-self.el
@@ -132,5 +132,20 @@
 				       isearch-mode-map)))
 
 
+;; Prompt before closing Emacs
+;; http://nileshk.com/2009/06/13/prompt-before-closing-emacs.html.
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed."
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+	  (save-buffers-kill-terminal)
+	(save-buffers-kill-emacs))
+    (message "Exit canceled")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
+
+
 (provide 'init-self)
 ;;; init-self.el ends here
