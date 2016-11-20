e2264517ea5b3cee6f0a5f8812d5a0b206b51463
Lingchao Xin <douglarek@gmail.com>
2016-11-20 13:15:14 +0800

    Rewrite melpa config with use-package

--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -7,157 +7,158 @@
 ;;; Code:
 
 (require 'package)
-(setq package-archives '(("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/") ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu"   . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
-;; Refresh the packages descriptions
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Refresh and install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; List of packages to load
-(setq package-load-list '(all))
+;; A use-package declaration for simplifying your .emacs
+(use-package use-package :config (setq use-package-always-ensure t))
 
-;; Install all needed
-(defvar my-packages '(ac-emoji
-		      ac-geiser
-		      auto-complete-c-headers
-		      aggressive-indent
-		      avy
-		      base16-theme
-		      bing-dict
-		      cider
-		      clj-refactor
-		      clojure-mode
-		      company
-		      counsel
-		      disable-mouse
-		      exec-path-from-shell
-		      flycheck
-		      geiser
-		      git-gutter
-		      git-messenger
-		      go-autocomplete
-		      go-eldoc
-		      go-impl
-		      golint
-		      go-guru
-		      go-mode
-		      go-rename
-		      iedit
-		      imenu-anywhere
-		      ix
-		      inf-clojure
-		      jedi
-		      lua-mode
-		      company-lua
-		      kotlin-mode
-		      magit
-		      merlin
-		      move-dup
-		      multiple-cursors
-		      paredit
-		      paren-face
-		      plur
-		      powerline
-		      projectile
-		      py-autopep8
-		      cargo
-		      flycheck-rust
-		      rainbow-delimiters
-		      restclient
-		      rust-mode
-		      smartparens
-		      smex
-		      swap-regions
-		      swiper
-		      tuareg
-		      undo-tree
-		      utop
-		      web-mode
-		      whole-line-or-region
-		      w3m
-		      xcscope
-		      yasnippet))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-refresh-contents)
-    (package-install p)))
+;; Minimalists' Bing dictionary for Emacs
+(use-package bing-dict)
+
+
+;; An emacs client to http://ix.io pastebin
+(use-package ix)
+
+
+;; Kotlin major mode for Emacs
+(use-package kotlin-mode :defer t)
+
+
+;; It's Magit! A Git Porcelain inside Emacs. https://magit.vc
+(use-package magit)
+
+
+;; Emacs minor mode for Eclipse-like moving and duplications of lines or selections with convenient key bindings
+(use-package move-dup)
+
+
+;; Multiple cursors for emacs.
+(use-package multiple-cursors)
+
+
+;; A minor mode for performing structured editing of S-expression data
+(use-package paredit :defer t)
+
+
+;; Easily (I hope) search and replace multiple variants of a word
+(use-package plur)
+
+
+;; HTTP REST client tool for emacs
+(use-package restclient)
+
+
+;; Swapping two regions of text in Emacs
+(use-package swap-regions)
 
 
 ;; Jump to things in Emacs tree-style
-(define-key global-map (kbd "C-c SPC") 'avy-goto-char)
-(global-set-key (kbd "M-g l") 'avy-goto-line)
+(use-package avy
+  :bind (("C-c SPC" . avy-goto-char)
+	 ("M-g l" . avy-goto-line))
+  )
 
 
 ;; Make Emacs use the $PATH set up by the user's shell
-(when (memq window-system '(mac ns))
+(use-package exec-path-from-shell
+  :when (memq window-system '(mac ns))
+  :config
   (exec-path-from-shell-initialize)
   (let ((envs '("GOROOT" "GOPATH" "PYTHONPATH")))
-    (exec-path-from-shell-copy-envs envs)))
+    (exec-path-from-shell-copy-envs envs))
+  )
 
 
 ;; Flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
-(setq flycheck-check-syntax-automatically '(save))
+(use-package flycheck
+  :diminish flycheck-mode
+  :init (add-hook 'after-init-hook #'global-flycheck-mode)
+  :config (setq flycheck-check-syntax-automatically '(save)))
 
 ;; git-gutter -> Emacs port of GitGutter which is Sublime Text Plugin
-(global-git-gutter-mode t)
+(use-package git-gutter
+  :diminish git-gutter-mode
+  :config (global-git-gutter-mode t))
 
 
 ;; ido/helm imenu tag selection across all buffers with the same mode
-(global-set-key (kbd "C-c C-j") 'imenu-anywhere)
+(use-package imenu-anywhere
+  :bind ("C-c C-j" . imenu-anywhere))
 
 
 ;; Emacs Port of git-messenger.vim
-(require 'git-messenger)
-(add-hook 'git-messenger:popup-buffer-hook 'magit-commit-mode)
-(global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
-(define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
+(use-package git-messenger
+  :bind (("C-x v p" . git-messenger:popup-message)
+	 :map git-messenger-map
+	 ("m" . git-messenger:copy-message))
+  :init (add-hook 'git-messenger:popup-buffer-hook 'magit-commit-mode)
+  :config (use-package magit))
 
 
 ;; An improved Go mode for emacs
-(add-hook 'go-mode-hook '(lambda ()
-			   (local-set-key (kbd "M-.") 'godef-jump) ; "M-*" back
-			   (make-local-variable 'before-save-hook)
-			   (setq gofmt-command "goimports")
-			   (setq flycheck-disabled-checkers '(go-errcheck))
-			   (add-hook 'before-save-hook 'gofmt-before-save)
-			   (define-key go-mode-map (kbd "C-c C-j") nil)
-			   (yas-minor-mode 1)))
+(use-package go-mode
+  :defer t
+  :init
+  (add-hook 'go-mode-hook (lambda ()
+			    (use-package go-impl)
+			    (use-package golint)
+			    (use-package go-guru)
+			    (use-package go-rename)
+			    (local-set-key (kbd "M-.") 'godef-jump) ; "M-*" back
+			    (make-local-variable 'before-save-hook)
+			    (setq gofmt-command "goimports")
+			    (setq flycheck-disabled-checkers '(go-errcheck))
+			    (add-hook 'before-save-hook 'gofmt-before-save)
+			    (define-key go-mode-map (kbd "C-c C-j") nil)
+			    (yas-minor-mode 1)
+			    (go-eldoc-setup)))
+  :config
+  (use-package go-autocomplete)
+  (use-package go-eldoc :diminish eldoc-mode))
 
-;; Go completion for Emacs
-(require 'go-autocomplete)
-
-;; Eldoc for Go
-(require 'go-eldoc)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
+(use-package go-autocomplete
+  :defer t
+  :config (use-package auto-complete))
 
 
 ;; Project Interaction Library for Emacs
-(projectile-global-mode)
+(use-package projectile
+  :config (projectile-global-mode))
 
 
 ;; Minor mode for Emacs that deals with parens pairs and tries to be smart about it
-(smartparens-global-mode t)
+(use-package smartparens
+  :diminish smartparens-mode
+  :config (smartparens-global-mode t))
 
 
 ;; smex -> A smart M-x enhancement for Emacs.
-(require 'smex)
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(use-package smex
+  :bind (("M-x" . smex)
+	 ("M-X" . smex-major-mode-commands)
+	 ;; This is your old M-x.
+	 ("C-c C-c M-x" . execute-extended-command))
+  :config (smex-initialize))
 
 
 ;; Treat undo history as a tree
-(global-undo-tree-mode t)
+(use-package undo-tree
+  :diminish undo-tree-mode
+  :config (global-undo-tree-mode t))
 
 
 ;; Operate on current line if region undefined
-(whole-line-or-region-mode t)
+(use-package whole-line-or-region
+  :diminish whole-line-or-region-mode
+  :config (whole-line-or-region-mode t))
 
 
 ;; Yasnippet -> A template system for Emacs
@@ -176,30 +177,48 @@
 (defvar my-snippets (joindirs user-emacs-directory "snippets" "my-snippets"))
 (setq yas-snippet-dirs '(yasnippet-snippets my-snippets go-snippets))
 
-(require 'yasnippet)
-(yas-reload-all)
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :config (yas-reload-all))
 
 
 ;; An Intelligent auto-completion extension for Emacs
-(require 'auto-complete-config)
-(ac-config-default)
-(define-key ac-completing-map "\r" 'ac-complete)
-(define-key ac-completing-map "\t" nil)
+(use-package auto-complete
+  :diminish auto-complete-mode
+  :init (auto-complete-mode t)
+  :bind (:map ac-completing-map
+	      ("C-p" . ac-previous)
+	      ("C-n" . ac-next)
+	      ("\r" . ac-complete)
+	      ("\t" . nil))
+  :config
+  (use-package ac-emoji)
+  (ac-config-default))
 
 
 ;; Emacs isearch with an overview. Oh, man!
-(ivy-mode 1)
-(global-set-key "\C-s" 'swiper)
-(advice-add 'swiper :after '(lambda () (recenter)))
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(use-package ivy
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) "))
+
+(use-package swiper
+  :bind ("C-s" . swiper)
+  :config (advice-add 'swiper :after '(lambda () (recenter))))
+
+(use-package counsel
+  :bind (("C-c g" . counsel-git)
+	 ("C-c j" . counsel-git-grep)))
 
 
 ;; web-mode.el is an autonomous emacs major-mode for editing web templates
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-hook 'web-mode-hook '(lambda ()
+(use-package web-mode
+  :defer t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-hook 'web-mode-hook '(lambda ()
 			    (setq web-mode-enable-auto-pairing t)
 			    (setq web-mode-enable-auto-closing t)
 			    (setq web-mode-enable-css-colorization t)
@@ -207,94 +226,172 @@
 				  '(("css" . (ac-source-css-property))
 				    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
 			    (local-set-key (kbd "RET") 'newline-and-indent)
-			    (define-key web-mode-map (kbd "C-n") 'web-mode-tag-match)))
+			    (define-key web-mode-map (kbd "C-n") 'web-mode-tag-match))))
 
 
 ;; Emacs and scheme talk to each other
-(setq geiser-active-implementations '(racket))
-(add-hook 'geiser-mode-hook 'ac-geiser-setup)
-(add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'geiser-repl-mode))
-(add-hook 'scheme-mode-hook #'enable-paredit-mode)
+(use-package ac-geiser
+  :defer t
+  :config
+  (add-hook 'geiser-mode-hook 'ac-geiser-setup)
+  (add-hook 'geiser-repl-mode-hook 'ac-geiser-setup))
+
+(use-package geiser
+  :defer t
+  :config
+  (setq geiser-active-implementations '(racket))
+  (use-package ac-geiser)
+  (eval-after-load "auto-complete" '(add-to-list 'ac-modes 'geiser-repl-mode))
+  (add-hook 'scheme-mode-hook #'enable-paredit-mode))
 
 
 ;; A face dedicated to lisp parentheses
-(global-paren-face-mode)
+(use-package paren-face
+  :config (global-paren-face-mode))
 
 
 ;; Emacs powerline and base16 theme
-(when (display-graphic-p)
-  (powerline-default-theme)
+(use-package base16-theme
+  :when (display-graphic-p)
+  :config
+  (use-package powerline)
   (load-theme 'base16-eighties t))
+
+(use-package powerline
+  :defer t
+  :config (powerline-default-theme))
 
 
 ;; Emacs OCaml mode
-(add-hook 'tuareg-mode-hook 'merlin-mode)
-(add-hook 'tuareg-mode-hook #'yas-minor-mode)
-(add-hook 'caml-mode-hook 'merlin-mode)
-(setq merlin-ac-setup t)
-(add-hook 'merlin-mode-hook (lambda ()
-			      (local-set-key (kbd "M-.") 'merlin-locate)
-			      (local-set-key (kbd "M-,") 'merlin-pop-stack)))
-(setq utop-command "opam config exec -- utop -emacs")
-(add-hook 'tuareg-mode-hook 'utop-minor-mode)
+(use-package tuareg
+  :defer t
+  :config
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (add-hook 'tuareg-mode-hook #'yas-minor-mode)
+  (use-package merlin)
+  (use-package utop))
+
+(use-package merlin
+  :defer t
+  :config
+  (setq merlin-ac-setup t)
+  (add-hook 'merlin-mode-hook (lambda ()
+				(local-set-key (kbd "M-.") 'merlin-locate)
+				(local-set-key (kbd "M-,") 'merlin-pop-stack)))
+  (add-hook 'caml-mode-hook 'merlin-mode))
+
+(use-package utop
+  :defer t
+  :config
+  (setq utop-command "opam config exec -- utop -emacs")
+  (add-hook 'tuareg-mode-hook 'utop-minor-mode))
 
 
 ;; Emacs configuration for Rust
-(add-hook 'rust-mode-hook '(lambda ()
-			     (yas-minor-mode 1)
-			     (rust-enable-format-on-save)
-			     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+(use-package rust-mode
+  :defer t
+  :config
+  (use-package cargo)
+  (use-package flycheck-rust)
+  (add-hook 'rust-mode-hook '(lambda ()
+			       (yas-minor-mode 1)
+			       (rust-enable-format-on-save)
+			       (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))))
+
+(use-package flycheck-rust :defer t)
 
 
 ;; Emacs support for the Clojure(Script) programming language
-(add-hook 'clojure-mode-hook '(lambda ()
-				(subword-mode)
-				(paredit-mode)
-				(rainbow-delimiters-mode)
-				(aggressive-indent-mode)
-				(inf-clojure-minor-mode)
-				(eldoc-mode)
-				(clj-refactor-mode 1)
-				(yas-minor-mode 1)
-				(setq cider-repl-display-help-banner nil)
-				(setq cider-repl-pop-to-buffer-on-connect nil)
-				(cljr-add-keybindings-with-prefix "C-c C-m")))
-(add-hook 'inf-clojure-mode-hook #'eldoc-mode)
+(use-package clojure-mode
+  :defer t
+  :config
+  (use-package rainbow-delimiters)
+  (use-package aggressive-indent)
+  (use-package inf-clojure)
+  (use-package clj-refactor)
+  (use-package yasnippet)
+  (use-package cider)
+  (add-hook 'clojure-mode-hook '(lambda ()
+				  (subword-mode)
+				  (paredit-mode)
+				  (rainbow-delimiters-mode)
+				  (aggressive-indent-mode)
+				  (inf-clojure-minor-mode)
+				  (eldoc-mode)
+				  (clj-refactor-mode 1)
+				  (yas-minor-mode 1)
+				  (setq cider-repl-display-help-banner nil)
+				  (setq cider-repl-pop-to-buffer-on-connect nil)
+				  (cljr-add-keybindings-with-prefix "C-c C-m")))
+  (add-hook 'inf-clojure-mode-hook #'eldoc-mode))
 
 
 ;; Emacs as a C/C++ Editor/IDE
-(defun my:ac-c-headers-init ()
-  "."
-  (require 'auto-complete-c-headers)
-  (add-to-list 'ac-sources 'ac-source-c-headers))
+(use-package auto-complete-c-headers
+  :defer t
+  :config
+  (defun my:ac-c-headers-init ()
+    "."
+    (require 'auto-complete-c-headers)
+    (add-to-list 'ac-sources 'ac-source-c-headers))
+  (my:ac-c-headers-init))
+
+(use-package xcscope
+  :defer t
+  :config
+  (cscope-setup))
+
 (add-hook 'c-mode-hook '(lambda ()
-			  (my:ac-c-headers-init)
+			  (use-package auto-complete-c-headers)
 			  (yas-minor-mode 1)
+			  (use-package iedit)
 			  (local-set-key (kbd "C-c ;") 'iedit-mode)
-			  (cscope-setup)))
+			  (use-package xcscope)))
 
 
 ;; Emacs major mode for editing Lua
-(add-hook 'lua-mode-hook '(lambda ()
-			   (company-mode)
-			   (setq-local company-backends '(company-lua))
-			   (yas-minor-mode)))
+(use-package company
+  :defer t
+  :config
+  (company-mode))
 
+(use-package lua-mode
+  :config
+  (add-hook 'lua-mode-hook '(lambda ()
+			      (use-package company)
+			      (use-package company-lua)
+			      (setq-local company-backends '(company-lua))
+			      (use-package yasnippet)
+			      (yas-minor-mode))))
 
 ;; Python auto-completion for Emacs
+(use-package jedi
+  :defer t
+  :bind (:map jedi-mode-map
+	      ("M-." . jedi:goto-definition)
+	      ("M-," . jedi:goto-definition-pop-marker)
+	      ("C-c d" . jedi:show-doc))
+  :config
+  (jedi:setup)
+  (setq jedi:complete-on-dot t))
+
+(use-package py-autopep8
+  :defer t
+  :config
+  (py-autopep8-enable-on-save)
+  (setq py-autopep8-options '("--max-line-length=120")))
+
 (add-hook 'python-mode-hook '(lambda ()
-			       (jedi:setup)
-			       (setq jedi:complete-on-dot t)
-			       (local-set-key (kbd "M-.") 'jedi:goto-definition)
-			       (local-set-key (kbd "M-,") 'goto-definition-pop-marker)
-			       (local-set-key (kbd "C-c d") 'jedi:show-doc)
+			       (use-package jedi)
+			       (use-package yasnippet)
 			       (yas-minor-mode)
-			       (py-autopep8-enable-on-save)
-			       (setq py-autopep8-options '("--max-line-length=120"))))
+			       (use-package py-autopep8)))
+
 
 ;; Disable the mouse in Emacs
-(global-disable-mouse-mode)
+(use-package disable-mouse
+  :config
+  (global-disable-mouse-mode))
 
 
 (provide 'init-melpa)
